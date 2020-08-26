2d58e8968a26cd35425a56d6d478e434a8f54732
STEPH SAKAI <stephaniesakai@stephanies-mbp.attlocal.net>
2020-08-25 18:49:48 -0700

    added responsive image sizing and background color on logo image

--- 14.3_Ajax_Giphy_Party/app.js
+++ 14.3_Ajax_Giphy_Party/app.js
@@ -2,10 +2,10 @@
 // https://developers.giphy.com/docs/api/endpoint/#search
 // API KEY: 4Q2j1blBfgQb8hTfuXWdWnKIqlxQxJLw
 // REQUIREMENTS:
-// [ ] Allow the user to search for a GIF and when the form is submitted, make an AJAX request to the Giphy API and return a single GIF
-// [ ] Once the Giphy API has responded with data, append the GIF to the page
-// [ ] Allow the user to search for as many GIFs as they would like and keep appending them to the page
-// [ ] Allow the user to remove all of the GIFs by clicking a button
+// - Allow the user to search for a GIF and when the form is submitted, make an AJAX request to the Giphy API and return a single GIF
+// - Once the Giphy API has responded with data, append the GIF to the page
+// - Allow the user to search for as many GIFs as they would like and keep appending them to the page
+// - Allow the user to remove all of the GIFs by clicking a button
 // PART 1:
 jQuery(function ($) {
 
